e7a468e5371db7e604e7ed41d4bc0584e31f3367
Andy Gozas <andy@gozas.me>
2021-01-27 07:48:54 +0000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+extern crate dusk_api;
+extern crate dusk_syntax;
+
+use std::any::{Any, TypeId};
+use dusk_api::{Freight, export_plugin, Function, RuntimeError};
+
+export_plugin!("precompiler", "0.0.0", Precompiler);
+
+pub struct Precompiler;
+
+impl Freight for Precompiler {
+    fn call_function (
+        self: &mut Self,
+        function_number: u64,
+        mut args: Vec<&mut Box<dyn Any>>
+        ) -> Result<Box<dyn Any>, RuntimeError> {
+        Ok(Box::new(0))
+    }
+
+    fn get_function_list (self: &mut Self) -> Vec<Function> {
+        let mut result: Vec<Function> = Vec::new();
+        Vec::new()
+    }
+}
